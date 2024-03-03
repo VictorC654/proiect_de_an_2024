1cfaaf7afacbd25570d1b7e0e36d681cd31f9836
mariusbvl <mariusbivol2003@gmail.com>
2024-03-03 16:51:29 +0200

    Am integrat BussinesLogic

---
+++ symptomSage.Domain/Entities/User/ULoginData.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace syptomSage.Domain.Entities.User
+{
+    public class ULoginData
+    {
+        public string Credential
+        {
+            get; set;
+        }
+        public string Password
+        {
+            get; set;
+        }
+        public string LoginIP
+        {
+            get; set;
+        }
+        public DateTime LoginDateTime
+        {
+            get; set;
+        }
+
+    }
+}
